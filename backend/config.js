f20203f28b26413a004c9b088516429e0c76e558
justdoit-4head <officialsoap11@gmail.com>
2024-04-30 12:47:39 +0530

    creating dotenv for extra security layer

--- backend/config.js
+++
@@ -1,5 +0,0 @@
-export const PORT = 5555;
-
-export const mongoDBURL =
-  'mongodb+srv://soap:TijQZyLMYRuw3a5a@cluster0.88zmhbg.mongodb.net/';
-
--- backend/index.js
+++ backend/index.js
@@ -1,39 +1,42 @@
 import express from 'express';
-import { PORT, mongoDBURL } from './config.js';
 import mongoose from 'mongoose';
 import booksRoute from './routes/booksRoute.js';
 import cors from 'cors';
+import dotenv from 'dotenv';
+
 
 const app = express();
+
+dotenv.config();
 
 // Middleware for parsing request body
 app.use(express.json());
 
 // Middleware for handling CORS POLICY
-// Option 1: Allow All Origins with Default of cors(*)
+
 app.use(cors());
 // Option 2: Allow Custom Origins
-// app.use(
-//   cors({
-//     origin: 'http://localhost:3000',
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//     allowedHeaders: ['Content-Type'],
-//   })
-// );
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type'],
+  })
+);
 
 app.get('/', (request, response) => {
   console.log(request);
-  return response.status(234).send('Welcome To MERN Stack Tutorial');
+  return response.status(234).send('Backend for the booklisting app!');
 });
 
 app.use('/books', booksRoute);
 
 mongoose
-  .connect(mongoDBURL)
+  .connect(process.env.mongoDBURL)
   .then(() => {
     console.log('App connected to database');
-    app.listen(PORT, () => {
-      console.log(`App is listening to port: ${PORT}`);
+    app.listen(process.env.PORT, () => {
+      console.log(`App is listening to port: ${process.env.PORT}`);
     });
   })
   .catch((error) => {
