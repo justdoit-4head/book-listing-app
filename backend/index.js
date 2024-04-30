f1e1f30d6141d8100b420ba8559b32b15b68398f
justdoit-4head <officialsoap11@gmail.com>
2024-04-30 13:21:37 +0530

    cors any origin

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(cors());
 // Option 2: Allow Custom Origins
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type'],
   })
